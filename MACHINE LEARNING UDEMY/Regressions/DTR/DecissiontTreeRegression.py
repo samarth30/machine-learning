5b5107af5a04629dd430bf7999b5f894e05eeab9
samarth <samarth.gugnani30@gmail.com>
2019-12-16 23:48:37 +0530

    dtr added

---
+++ MACHINE LEARNING UDEMY/Regressions/DTR/DecissiontTreeRegression.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  3 02:35:57 2019
+
+@author: DELL
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# importing the dataset
+
+dataset = pd.read_csv('Position_Salaries.csv')
+x = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+
+#fitting the Dtr in the model
+from sklearn.tree import DecisionTreeRegressor
+regressor = DecisionTreeRegressor(random_state=0)
+regressor.fit(x,y)
+
+#predict the sallary
+y_pred = regressor.predict(6.5)
+
+# graph of plynomial regression
+x_grid = np.arange(min(x),max(x),0.01)
+x_grid = x_grid.reshape(len(x_grid),1)
+plt.scatter(x,y,color="red")
+plt.plot(x_grid,regressor.predict(x_grid),color="blue")
+plt.title("truth or bluff (Decission Tree regression)")
+plt.xlabel("position level")
+plt.ylabel("sallary")
+plt.show()
+
+
